7e55c2ef2cfaec313b83a45d5900be1d0b3e9ad1
Matias Mingo <mamingo@uc.cl>
2019-03-15 21:14:14 -0300

    Nueva recopilación 2019-03-15 21:14:13.705569

--- control_versiones_automatico_gitpython.py
+++ control_versiones_automatico_gitpython.py
@@ -3,7 +3,7 @@ import datetime
 
 
 def subir_version():
-    rw_dir = '/Users/matiasmingo/Desktop/Banco Central/A:PEGA/Tareas/Recopilación y filtración noticias/Code'
+    rw_dir = '../Code'
     repo = Repo(rw_dir)
     repo.git.add(update=True)
     repo.git.commit('-m', 'Nueva recopilación {}'.format(datetime.datetime.now()), author='mamingo@uc.cl')
