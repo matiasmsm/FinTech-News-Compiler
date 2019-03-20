670d38fd6c1ed4e85b4f905b0f92c199549f03d1
Matias Mingo <mamingo@uc.cl>
2019-03-20 17:46:13 -0300

    Nueva recopilación 2019-03-20 17:46:12.457541

--- pinboardOT.py
+++ pinboardOT.py
@@ -5,6 +5,9 @@ pb = pinboard.Pinboard('lsanz_observatorio:CC8E7F9E71CC9B935378')
 
 def obtener_posts():
     posts = pb.posts.recent(tag=["publishedOT", "d:201903"])
-    #print(posts)
     for k,v in posts.items():
         print(k,v)
+
+
+if __name__ == '__main__':
+    obtener_posts()
